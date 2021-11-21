ddce2039e7740d940ca0992220c2c70ca13912ae
The Shadow Brokers <90165013+TheShadowBrokers1@users.noreply.github.com>
2021-11-20 19:17:14 -0600

    Update Spotify.js

--- plugins/Spotify.js
+++ plugins/Spotify.js
@@ -1,27 +1,27 @@
 // Made By github.com/TOXIC-DEVIL
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `Masukkan judul musik!`
+  if (!text) throw `Ingresa el título de la música!`
   let res = await fetch(global.API('zeks', '/api/spotify', { q: text }, 'apikey'))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if(!json.data[0]) throw json
   let { title, artists, album, thumb, url, preview_mp3 } = json.data[0]
-let spotifyinfo = '❒═════❬ SPOTIFY ❭═════╾❒
+let spotifyinfo = `❒═════❬ SPOTIFY ❭═════╾❒
 ┬
-├‣✨ *Titulo:* ${title}
+├‣✨ Titulo: ${title}
 ┴
 ┬
-├‣🗣️ *Artista:* ${artists}
+├‣🗣️ Artista: ${artists}
 ┴
 ┬
-├‣🎆 *Album:* ${album}
+├‣🎆 Album: ${album}
 ┴
 ┬
-├‣🌐 *URL*: ${url}
+├‣🌐 URL: ${url}
 ┴
 ┬
-├‣💚 *Direct URL:* ${preview_mp3}\n┴\n\n*_- Enviando música de previsualización_*\n\n_﹫ᴛʜᴇ Sʜᴀᴅᴏᴡ ʙʀᴏᴋᴇʀs ﹣ ʙᴏᴛ_`
+├‣💚 Direct URL: ${preview_mp3}\n┴\n\n*- Enviando música de previsualización*\n\n_﹫ᴛʜᴇ Sʜᴀᴅᴏᴡ ʙʀᴏᴋᴇʀs ﹣ ʙᴏᴛ_`
 
   await conn.sendFile(m.chat, thumb, '', spotifyinfo, m)
   await conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyinfo, m)
